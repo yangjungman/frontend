58d1aa8b4d4cafc5640be8d99712439f89a4adce
drv98 <moahair89@naver.com>
2023-07-11 14:57:14 +0900

    액션버튼추가

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -22,6 +22,7 @@ function Home() {
             <th scope="col">이름</th>
             <th scope="col">유저네임</th>
             <th scope="col">이메일</th>
+            <th scope="col">액션</th>
           </tr>
         </thead>
         <tbody>
@@ -31,6 +32,11 @@ function Home() {
               <td>{user.name}</td>
               <td>{user.username}</td>
               <td>{user.email}</td>
+              <td>
+                <button className="btn btn-outline-secondary mx-2">보기</button>
+                <button className="btn btn-outline-warning mx-2">수정</button>
+                <button className="btn btn-outline-danger mx-2">삭제</button>
+              </td>
             </tr>
           ))}
         </tbody>
