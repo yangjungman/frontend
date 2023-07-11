4ecb71610d181c0726258558ada04a9b79b288b4
drv98 <moahair89@naver.com>
2023-07-11 14:00:28 +0900

    테이블추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Navbar from "./layout/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/users/ViewUser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ViewUser() {
+  return <div>ViewUser</div>;
+}
+
+export default ViewUser;
